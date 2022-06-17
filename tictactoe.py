050ee71390544f00ec5597502d2924cf233594e5
Aj Yballa <79114896+ajyballa@users.noreply.github.com>
2022-06-17 12:36:58 -0600

    Update tictactoe.py
    Switching back from Java back to Python is a weird experience, but it does make things a bit easier. Also I realized i made the rows have 4 boxes instead of 3. Whoops.

--- tictactoe.py
+++ tictactoe.py
@@ -2,11 +2,17 @@
 #created on 6/14/2022
 
 #Variables
-line = "----------------"
-boxes = "   |   |   |   "
+line =  "-----------"
+boxes = "   |   |   "
 player_move = " x "
 computer_move = " O "
 win_con = False
+
+clear_row = ["   |   |   "]
+row_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#1 2 3
+#4 5 6
+#7 8 9
 
 #this game runs inside of a loop, checking to see if player wants to play.
 while True:
@@ -19,11 +25,22 @@ while True:
         print(boxes)
 
 
-
+        #Loop to check if anyone has won yet, if not, keep playing.
         while win_con == False:
-            player_choice = input("Make your move! enter 1-9, 1 being the top left corner, 9 the bottom right.")
+            player_choice = int(input("Make your move! enter 1-9, 1 being the top left corner, 9 the bottom right. "))
             #TODO use arrays/lists and put player choice into existing list, same with computer
+            if player_choice in row_list:
 
+
+
+                #debug command to check if player chose a valid number
+                print("Player chose " + str(player_choice) + " at index " + str(row_list.index(player_choice)))
+            
+                continue
+            #debug command to check if player chose a valid number
+            elif player_choice not in row_list:
+                print("Whoops! Choose a number 1-9.")
+                continue
 
         
     elif play == 'q' or play == 'Q':
